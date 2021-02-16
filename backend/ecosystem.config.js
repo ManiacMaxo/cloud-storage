bb243794e9e1afd280989da4e670dc105e2aecd4
Victor Gorchilov <victor@gorchilov.com>
2021-02-16 12:37:45 +0000

    [fix] watch on backend causing cors failure

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     name: 'cloud storage backend',
     script: 'src/index.ts',
-    watch: true,
     exp_backoff_restart_delay: 100,
     error_file: 'err.log',
     log_file: 'access.log',
